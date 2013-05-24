09f571d13b85afac0a759926461e1fa94e22b176
David S. Nunez <david@davidnunez.com>
2013-05-24 15:08:50 -0400

    moving files

--- data_processing/funf/db_import.rb
+++ data_processing/funf/db_import.rb
@@ -13,21 +13,37 @@ require 'csv'
 require 'sqlite3'
 require 'parseconfig'
 require 'mongo'
+require 'json'
+include Mongo
 
 mongo_client = MongoClient.new("localhost", 27017)
-files_coll = mongo_client.db("gsu").db["files"]
-devices_coll = mongo_client.db("gsu").db["devices"]
+files_coll = mongo_client.db("gsu")["files"]
+devices_coll = mongo_client.db("gsu")["devices"]
+data_coll = mongo_client.db("gsu")["data"]
+
+files_coll.remove
+devices_coll.remove
+data_coll.remove
+
 
 dir_contents = Dir["*.db"]
 
-dir_contents.first(10000).each do |f| 
+dir_contents.first(1000).each do |f| 
 	begin
-		puts "Decrypting: " + f
-		system "~/data_processing/bin/dbdecrypt.py -p 'changeme' #{f}"
+
 			
 	 	db = SQLite3::Database.new( f )
+		if files_coll.find('filename' => f).to_a.length != 0
+			next
+		end
+
+		puts "Processing: " + f
+		system "~/data_processing/bin/dbdecrypt.py -p 'changeme' #{f}"
+
 	  	file_info = db.get_first_row( "select * from file_info" )
 	  	file_id, database_name, device_id, uuid, created = file_info
+
+
 
 	  	# Example Filename
 	  	# 1362428241-00000174_0a7442444300a557_9385a79e-2d38-4917-8b67-8e600d1ba226_1358991911_mainPipeline.db
@@ -37,11 +53,19 @@ dir_contents.first(10000).each do |f|
 			raise "Invalid filename"
 		end
 
-		upload_date = filename_metadata[0].splite('-')[0] 
+		upload_date = filename_metadata[0].split('-')[0] 
 		ordinal_value = filename_metadata[0].split('-')[1]
 		serial_id = filename_metadata[1]
 		device_id = filename_metadata[2]
 		collected_date = filename_metadata[3]
+
+
+			if (upload_date.to_s.length == 10) 
+				upload_date = upload_date.to_i * 1000
+			end
+			if (collected_date.to_s.length == 10) 
+				collected_date = collected_date.to_i * 1000
+			end
 
 		puts "Mapping: #{device_id} to #{serial_id}"
 
@@ -50,45 +74,72 @@ dir_contents.first(10000).each do |f|
 			devices_coll.insert(doc)
 		end
 
-		puts "Storing File"
+		puts "Storing File Metadata"
 
 		doc = {"filename" => f,
 				"upload_date" => upload_date, 
 				"ordinal_value" => ordinal_value, 
 				"serial_id" => serial_id, 
-				"collected_date" => collected_date}
-
+				"collected_date" => collected_date,
+				"processed" => false}
 		files_coll.insert(doc);
 
 
-		# rows = merged_db.execute("SELECT serial_id FROM master_id WHERE serial_id=? and device_id=?",[serial_id, device_id])
 
-		# if rows.size == 0
-		# 	merged_db.execute("INSERT INTO master_id (serial_id, device_id, label, version) 
-	 #            VALUES (?, ?, ?, ?)", [serial_id, device_id, "", ""])
-		# 	puts "Added mapping."
-		# else
-		# 	puts "Mapping already added."
-		# end
-		
-		# rows = db.execute("select * from data") 
-		# rows.each do |row|
-		# 	data_id, probe, timestamp, value = row
-		# 	merged_id = uuid + '-' + data_id.to_s
+		puts "Storing Data"
+		rows = db.execute("select * from data") 
+		rows.each do |row|
+			data_id, probe, timestamp, value = row
 
-		# 	merged_db.execute("INSERT INTO data (id, device, probe, timestamp, value) 
-	 #            VALUES (?, ?, ?, ?, ?)", [merged_id, device_id, probe, timestamp, value])
+			if (timestamp.to_s.length == 10) 
+				timestamp *= 1000
+			end
 
-		# end
+			merged_id = uuid + '-' + data_id.to_s
+			doc = {"data_id" => merged_id,
+					"serial_id" => serial_id,
+					"probe" => probe,
+					"timestamp" => timestamp,
+					"value" => value }
+			begin 
+#				puts doc["value"]
+				## ANECDOTAL FIXES
+
+				if doc['probe'] == "LauncherApp" 
+					newValue = '{"app" : "' + doc["value"] + '"}'
+					doc["value"] = newValue
+				end
+				if doc['probe'] == "FileMoverService" 
+					doc["value"] += "}"
+				end
+
+				j = JSON.parse(doc['value'])
+				data_coll.insert(doc)
+			
+			rescue => detail
+				error_msg = "ERROR--------------------------------\n" +
+					"\t" + doc['probe'] + ': ' + detail + "\n" +
+					"\t" + doc.inspect + "\n" +
+					"-------------------------------------"
+				puts error_msg
+				#error_file.puts error_msg
+			end
+
+
+		end
+		files_coll.update({"filename" => f}, {"$set" => {"processed" => true}})
+
 		# processed_file.puts(f)
 	rescue => detail
 		#error_log_file.puts("ERROR: " + f + ': '+ detail)
-		puts ("ERROR--------------------------------")
-		puts ("\t" + f + ': ' + detail)
-		puts ("-------------------------------------")
-		#error_file.puts(f)
-
+		error_msg = "ERROR--------------------------------\n" +
+			"\t" + f + ': ' + detail + "\n" +
+			"-------------------------------------"
+		puts error_msg
+		#error_file.puts error_msg
 	ensure
 		db.close
 	end
+
 end
+
