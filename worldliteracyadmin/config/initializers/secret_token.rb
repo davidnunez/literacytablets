f6c8a90d3d2e6b38cbb75729bffda516fb98c1a0
David Nunez <david@davidnunez.com>
2013-06-16 14:39:18 -0400

    initial commit of worldliteracyadmin app

---
+++ worldliteracyadmin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worldliteracyadmin::Application.config.secret_token = 'f49ad06b6d33fdd303befe419ec301bf998a73d34b7cac3e318d9b26f16a987ef3e92419737659cb836b92af0a881d048781fcfadcc59aa871ffc9e3a83af5df'
