d1d16cf44aa4051c6b6151f3437b0e39baf2e1fe
David S. Nunez <david@davidnunez.com>
2013-06-21 15:30:33 -0400

    additional formats

--- data_processing/funf/db_archive.rb
+++ data_processing/funf/db_archive.rb
@@ -34,8 +34,8 @@ DATA_ERROR_PATH = config['DATA_ERROR_PATH']
 Dir.chdir DATA_RAW_PATH
 
 RUN_TIME = Time.now.to_i
-ERROR_FILE = File.open("#{RUN_TIME}_ERROR_ARCHIVE_FILE.txt", 'a')
-ERROR_LOG_FILE = File.open("#{RUN_TIME}_ERROR_ARCHIVE_LOG.txt", 'a')
+ERROR_FILE = File.open("./logs/#{RUN_TIME}_ERROR_ARCHIVE_FILE.txt", 'a')
+ERROR_LOG_FILE = File.open("./logs/#{RUN_TIME}_ERROR_ARCHIVE_LOG.txt", 'a')
 
 
 def log_error(detail, f)
@@ -121,6 +121,100 @@ dir_contents.each do |f|
 
 end
 
+
+puts "STARTING .DB.ORIG ARCHIVE"
+
+dir_contents = Dir["*.orig"]
+progress_index = 0
+dir_contents.each do |f| 
+	begin
+		# Example Filename
+	  	# 1347393204-ef62a74f-33c7-4dc5-9b7d-c5b225f7d82a_1347361242_mainPipeline.db.orig
+	  	# 1362680183-00001788_037c70c441e05597_a0421c26-5f73-423f-965f-9819f0e7c7b6_1362676624_mainPipeline.db.orig:
+		filename_metadata = f.split('_')
+		if filename_metadata.length != 5 then
+			raise "Invalid filename"
+		end
+
+		collected_date = filename_metadata[3]
+
+		if collected_date.length != 10 then
+			raise "Invalid date"
+		end
+
+
+
+		directory = DATA_ARCHIVE_PATH + "/" + DataFile.time_at_ms(collected_date.to_i).strftime("%Y-%m")
+		archive_file f, directory, progress_index
+	rescue => detail
+		log_error detail, f
+	end
+end
+
+puts "STARTING *TB*.txt ARCHIVE"
+
+dir_contents = Dir["*tb*.txt"]
+progress_index = 0
+dir_contents.each do |f| 
+	begin
+		# Example Filename
+	  	# 1355252982-tb-1350659613-161c110044010357.txt
+
+		filename_metadata = f.split('-')
+		if filename_metadata.length != 4 then
+			raise "Invalid filename"
+		end
+
+		collected_date = filename_metadata[2]
+
+		if collected_date.length != 10 then
+			raise "Invalid date"
+		end
+
+
+
+		directory = DATA_ARCHIVE_PATH + "/" + DataFile.time_at_ms(collected_date.to_i).strftime("%Y-%m")
+		archive_file f, directory, progress_index
+	rescue => detail
+		log_error detail, f
+	end
+end
+
+puts "STARTING *WLA*.txt ARCHIVE"
+
+dir_contents = Dir["*wla*.txt"]
+progress_index = 0
+dir_contents.each do |f| 
+	begin
+		# Example Filename
+	  	# 1355252982-wla-1350659613-161c110044010357.txt
+
+		filename_metadata = f.split('-')
+		if filename_metadata.length != 4 then
+			raise "Invalid filename"
+		end
+
+		collected_date = filename_metadata[2]
+
+		if collected_date.length != 10 then
+			raise "Invalid date"
+		end
+
+
+
+		directory = DATA_ARCHIVE_PATH + "/" + DataFile.time_at_ms(collected_date.to_i).strftime("%Y-%m")
+		archive_file f, directory, progress_index
+	rescue => detail
+		log_error detail, f
+	end
+end
+
+
+puts "MOVING .error files"
+dir_contents = Dir["*.error"]
+dir_contents.each do |f| 
+	FileUtils.mv(f, DATA_ERROR_PATH + "/" + f)
+end
 
 
 
